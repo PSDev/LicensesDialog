1413b2b5f6e242ee3e9ae60620bc8f6ec9cf19cf
Philip Schiffer <philip.schiffer@gmail.com>
2013-11-09 13:23:15 +0100

    Add missing copyright notice

--- library/src/main/java/de/psdev/licensesdialog/licenses/BSD3ClauseLicense.java
+++ library/src/main/java/de/psdev/licensesdialog/licenses/BSD3ClauseLicense.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Philip Schiffer
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package de.psdev.licensesdialog.licenses;
 
 import android.content.Context;
@@ -5,31 +21,31 @@ import de.psdev.licensesdialog.R;
 
 public class BSD3ClauseLicense extends License {
 
-	private static final long serialVersionUID = -5205394619884057474L;
+    private static final long serialVersionUID = -5205394619884057474L;
 
-	@Override
-	public String getName() {
-		return "BSD 3-Clause License";
-	}
+    @Override
+    public String getName() {
+        return "BSD 3-Clause License";
+    }
 
-	@Override
-	public String getSummaryText(final Context context) {
-		return getContent(context, R.raw.bsd3_summary);
-	}
+    @Override
+    public String getSummaryText(final Context context) {
+        return getContent(context, R.raw.bsd3_summary);
+    }
 
-	@Override
-	public String getFullText(final Context context) {
-		return getContent(context, R.raw.bsd3_full);
-	}
+    @Override
+    public String getFullText(final Context context) {
+        return getContent(context, R.raw.bsd3_full);
+    }
 
-	@Override
-	public String getVersion() {
-		return "";
-	}
+    @Override
+    public String getVersion() {
+        return "";
+    }
 
-	@Override
-	public String getUrl() {
-		return "http://opensource.org/licenses/BSD-3-Clause";
-	}
+    @Override
+    public String getUrl() {
+        return "http://opensource.org/licenses/BSD-3-Clause";
+    }
 
 }
