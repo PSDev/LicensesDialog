b9e3fa9d2c04a855ce64bad1215974b0a723ceb9
Peter Heisig <peter.heisig@medien-dresden.de>
2014-02-08 16:45:18 +0100

    new license to default

--- library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
+++ library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
@@ -16,14 +16,15 @@
 
 package de.psdev.licensesdialog;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
 import de.psdev.licensesdialog.licenses.BSD3ClauseLicense;
+import de.psdev.licensesdialog.licenses.CreativeCommonsAttributionNoDerivs30Unported;
 import de.psdev.licensesdialog.licenses.ISCLicense;
 import de.psdev.licensesdialog.licenses.License;
 import de.psdev.licensesdialog.licenses.MITLicense;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public final class LicenseResolver {
 
@@ -40,6 +41,7 @@ public final class LicenseResolver {
         registerLicense(new BSD3ClauseLicense());
         registerLicense(new ISCLicense());
         registerLicense(new MITLicense());
+        registerLicense(new CreativeCommonsAttributionNoDerivs30Unported());
     }
 
     /**
