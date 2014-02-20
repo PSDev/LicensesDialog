071e394e83a8649c0b6615e52e9efd6ad38dced0
Peter Heisig <peter.heisig@medien-dresden.de>
2014-02-20 08:57:40 +0100

    reordered imports

--- library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
+++ library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
@@ -16,15 +16,15 @@
 
 package de.psdev.licensesdialog;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
 import de.psdev.licensesdialog.licenses.BSD3ClauseLicense;
 import de.psdev.licensesdialog.licenses.CreativeCommonsAttributionNoDerivs30Unported;
 import de.psdev.licensesdialog.licenses.ISCLicense;
 import de.psdev.licensesdialog.licenses.License;
 import de.psdev.licensesdialog.licenses.MITLicense;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class LicenseResolver {
 
