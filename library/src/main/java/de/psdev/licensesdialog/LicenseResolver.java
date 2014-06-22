33989a43f7beeef4bde0adbae037e4b75aa98bca
Shaleen Jain <shaleen.jain95@gmail.com>
2014-06-23 00:05:55 +0530

    Added GNU GPLv3 License

--- library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
+++ library/src/main/java/de/psdev/licensesdialog/LicenseResolver.java
@@ -19,6 +19,7 @@ package de.psdev.licensesdialog;
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
 import de.psdev.licensesdialog.licenses.BSD3ClauseLicense;
 import de.psdev.licensesdialog.licenses.CreativeCommonsAttributionNoDerivs30Unported;
+import de.psdev.licensesdialog.licenses.GnuGeneralPublicLicense30;
 import de.psdev.licensesdialog.licenses.ISCLicense;
 import de.psdev.licensesdialog.licenses.License;
 import de.psdev.licensesdialog.licenses.MITLicense;
@@ -44,6 +45,7 @@ public final class LicenseResolver {
         registerLicense(new MITLicense());
         registerLicense(new GnuLesserGeneralPublicLicense21());
         registerLicense(new CreativeCommonsAttributionNoDerivs30Unported());
+        registerLicense(new GnuGeneralPublicLicense30());
     }
 
     /**
