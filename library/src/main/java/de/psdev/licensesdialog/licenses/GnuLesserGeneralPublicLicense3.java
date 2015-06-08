8a9fe357e13e96ee3640b6cb07fb4e036e8e9030
Philip Schiffer <philip.schiffer@gmail.com>
2015-06-08 09:40:37 +0200

    Fix copyright info

--- library/src/main/java/de/psdev/licensesdialog/licenses/GnuLesserGeneralPublicLicense3.java
+++ library/src/main/java/de/psdev/licensesdialog/licenses/GnuLesserGeneralPublicLicense3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Peter Heisig
+ * Copyright 2015 Jakob Schrettenbrunner
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
