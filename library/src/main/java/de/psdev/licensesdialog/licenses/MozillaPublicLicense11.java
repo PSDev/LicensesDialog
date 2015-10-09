813c6e53ea805b8487c4b413019a161658e86d06
Kai Winter <kaiwinter@gmx.de>
2015-10-09 19:35:10 +0200

    Added Mozilla Public License 1.1

---
+++ library/src/main/java/de/psdev/licensesdialog/licenses/MozillaPublicLicense11.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013 Philip Schiffer
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package de.psdev.licensesdialog.licenses;
+
+import android.content.Context;
+
+import de.psdev.licensesdialog.R;
+
+public class MozillaPublicLicense11 extends License {
+
+    private static final long serialVersionUID = -5912500033007492703L;
+
+    @Override
+    public String getName() {
+        return "Mozilla Public License 1.1";
+    }
+
+    @Override
+    public String readSummaryTextFromResources(final Context context) {
+        return getContent(context, R.raw.mpl_11_summary);
+    }
+
+    @Override
+    public String readFullTextFromResources(final Context context) {
+        return getContent(context, R.raw.mpl_11_full);
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.1";
+    }
+
+    @Override
+    public String getUrl() {
+        return "http://mozilla.org/MPL/1.1/";
+    }
+
+}
