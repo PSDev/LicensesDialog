e5647aebd41fa55732222f9795ccb77c3d5762e7
TacoTheDank <skytkrsfan3895@gmail.com>
2018-09-24 20:58:26 -0400

    sample
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- sample/src/main/java/de/psdev/licensesdialog/sample/SampleActivity.java
+++ sample/src/main/java/de/psdev/licensesdialog/sample/SampleActivity.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+
 import de.psdev.licensesdialog.LicensesDialog;
 import de.psdev.licensesdialog.LicensesDialogFragment;
 import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
@@ -31,133 +32,127 @@ import de.psdev.licensesdialog.model.Notices;
 
 public class SampleActivity extends FragmentActivity {
 
-    // ==========================================================================================================================
     // Android Lifecycle
-    // ==========================================================================================================================
-
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
     }
 
-    // ==========================================================================================================================
     // Public API
-    // ==========================================================================================================================
-
     public void onSingleClick(final View view) {
         final String name = "LicensesDialog";
-        final String url = "http://psdev.de";
+        final String url = "https://github.psdev.de/";
         final String copyright = "Copyright 2013 Philip Schiffer <admin@psdev.de>";
         final License license = new ApacheSoftwareLicense20();
         final Notice notice = new Notice(name, url, copyright, license);
         new LicensesDialog.Builder(this)
-            .setNotices(notice)
-            .build()
-            .show();
+                .setNotices(notice)
+                .build()
+                .show();
     }
 
     public void onMultipleClick(final View view) {
         new LicensesDialog.Builder(this)
-            .setNotices(R.raw.notices)
-            .build()
-            .show();
+                .setNotices(R.raw.notices)
+                .build()
+                .show();
     }
 
     public void onMultipleIncludeOwnClick(final View view) {
         new LicensesDialog.Builder(this)
-            .setNotices(R.raw.notices)
-            .setIncludeOwnLicense(true)
-            .build()
-            .show();
+                .setNotices(R.raw.notices)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show();
     }
 
     public void onMultipleProgrammaticClick(final View view) {
         final Notices notices = new Notices();
-        notices.addNotice(new Notice("Test 1", "http://example.org", "Example Person", new ApacheSoftwareLicense20()));
-        notices.addNotice(new Notice("Test 2", "http://example.org", "Example Person 2", new GnuLesserGeneralPublicLicense21()));
+        notices.addNotice(new Notice("Test 1", "https://example.org", "Example Person", new ApacheSoftwareLicense20()));
+        notices.addNotice(new Notice("Test 2", "https://example.org", "Example Person 2", new GnuLesserGeneralPublicLicense21()));
 
         new LicensesDialog.Builder(this)
-            .setNotices(notices)
-            .setIncludeOwnLicense(true)
-            .build()
-            .show();
+                .setNotices(notices)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show();
     }
 
     public void onSingleFragmentClick(final View view) {
         final String name = "LicensesDialog";
-        final String url = "http://psdev.de";
+        final String url = "https://github.psdev.de/";
         final String copyright = "Copyright 2013 Philip Schiffer <admin@psdev.de>";
         final License license = new ApacheSoftwareLicense20();
         final Notice notice = new Notice(name, url, copyright, license);
 
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotice(notice)
-            .setIncludeOwnLicense(false)
-            .build();
+                .setNotice(notice)
+                .setIncludeOwnLicense(false)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
 
     public void onMultipleFragmentClick(final View view) throws Exception {
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotices(R.raw.notices)
-            .build();
+                .setNotices(R.raw.notices)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
 
     public void onMultipleIncludeOwnFragmentClick(final View view) throws Exception {
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotices(R.raw.notices)
-            .setShowFullLicenseText(false)
-            .setIncludeOwnLicense(true)
-            .build();
+                .setNotices(R.raw.notices)
+                .setShowFullLicenseText(false)
+                .setIncludeOwnLicense(true)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
 
     public void onMultipleProgrammaticFragmentClick(final View view) {
         final Notices notices = new Notices();
-        notices.addNotice(new Notice("Test 1", "http://example.org", "Example Person", new ApacheSoftwareLicense20()));
-        notices.addNotice(new Notice("Test 2", "http://example.org", "Example Person 2", new GnuLesserGeneralPublicLicense21()));
+        notices.addNotice(new Notice("Test 1", "https://example.org", "Example Person", new ApacheSoftwareLicense20()));
+        notices.addNotice(new Notice("Test 2", "https://example.org", "Example Person 2", new GnuLesserGeneralPublicLicense21()));
 
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotices(notices)
-            .setShowFullLicenseText(false)
-            .setIncludeOwnLicense(true)
-            .build();
+                .setNotices(notices)
+                .setShowFullLicenseText(false)
+                .setIncludeOwnLicense(true)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
 
     public void onCustomThemeClick(final View view) {
         new LicensesDialog.Builder(this)
-            .setNotices(R.raw.notices)
-            .setIncludeOwnLicense(true)
-            .setThemeResourceId(R.style.custom_theme)
-            .setDividerColorId(R.color.custom_divider_color)
-            .build()
-            .show();
+                .setNotices(R.raw.notices)
+                .setIncludeOwnLicense(true)
+                .setThemeResourceId(R.style.custom_theme)
+                .setDividerColorId(R.color.custom_divider_color)
+                .build()
+                .show();
     }
 
     public void onCustomThemeFragmentClick(final View view) throws Exception {
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotices(R.raw.notices)
-            .setShowFullLicenseText(false)
-            .setIncludeOwnLicense(true)
-            .setThemeResourceId(R.style.custom_theme)
-            .setDividerColorRes(R.color.custom_divider_color)
-            .build();
+                .setNotices(R.raw.notices)
+                .setShowFullLicenseText(false)
+                .setIncludeOwnLicense(true)
+                .setThemeResourceId(R.style.custom_theme)
+                .setDividerColorRes(R.color.custom_divider_color)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
 
     public void onCustomCssStyleClick(final View view) {
         new LicensesDialog.Builder(this)
-            .setNotices(R.raw.notices)
-            .setNoticesCssStyle(R.string.coustom_notices_default_style)
-            .build()
-            .show();
+                .setNotices(R.raw.notices)
+                .setNoticesCssStyle(R.string.coustom_notices_default_style)
+                .build()
+                .show();
     }
 
     public void onCustomCssStyleFragmentClick(final View view) throws Exception {
@@ -171,9 +166,9 @@ public class SampleActivity extends FragmentActivity {
         String style = String.format(formatString, pBg, bodyBg, preBg, liColor, linkColor);
 
         final LicensesDialogFragment fragment = new LicensesDialogFragment.Builder(this)
-            .setNotices(R.raw.notices)
-            .setNoticesCssStyle(style)
-            .build();
+                .setNotices(R.raw.notices)
+                .setNoticesCssStyle(style)
+                .build();
 
         fragment.show(getSupportFragmentManager(), null);
     }
